ee52a60cebfb7e3f68e9cba907e88d7a5df9ac0f
Vitor <vitordossantoscorreia@gmail.com>
2026-05-14 17:25:31 -0300

    fix: ajustar bufferAttribute para o build

--- src/components/3d/Nebula.tsx
+++ src/components/3d/Nebula.tsx
@@ -43,12 +43,14 @@ export function Nebula() {
           count={positions.length / 3}
           array={positions}
           itemSize={3}
+          args = {[positions, 3]}
         />
         <bufferAttribute
           attach="attributes-color"
           count={colors.length / 3}
           array={colors}
           itemSize={3}
+          args={[colors, 3]}
         />
       </bufferGeometry>
       {/* O Material da Nebulosa: Brilhante, transparente e macio */}
